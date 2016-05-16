ac519709ba7ef6db82604a77728402bcf71c9a79
Lode Claassen <lode@alsvanzelf.nl>
2016-05-16 20:44:03 +0200

    make better available via composer and cleanup code

--- README.md
+++ README.md
@@ -1,4 +1,67 @@
-linkify
-=======
+# Linkify
 
-Finding and linkifying things that look like links
+Finding and linkifying things that look like links.
+
+It focuses on: a main use case, short and fast code, and code-sharing between backend and frontend.
+
+This package works for multiple languages, right now php and js.
+
+
+## Install
+
+[Use Composer](http://getcomposer.org/). And use require to get the latest stable version:
+
+```
+composer require alsvanzelf/linkify:dev-master
+```
+
+
+## Features
+
+The basics:
+
+- Anything that starts with `http://`, `https://` or `www.`
+- No support for ftp, email addresses, basic auth, and other edge cases.
+- No advanced validation, for example real tld's.
+- Adds `http://` for the href when it was not given in the text.
+
+Things that work:
+
+- Case insensitive
+- Unicode
+- Paths, query strings, hashes, and other allowed characters
+- Urls starting/ending a line or the whole string
+- Not including punctuation after the url
+- Urls enclosed in brackets
+- Urls with brackets inside the url
+
+Things that -currently- do not work:
+
+- Urls which contain one of: @ |
+
+
+## Usage
+
+``` php
+$linkifier = new alsvanzelf\linkify\linkify();
+$linkified = $linkifier->linkify($plain);
+```
+
+``` js
+var linkified = linkify(plain);
+```
+
+Note the input string should NOT contain html to prevent XSS.
+As afterwards one can not differentiate the added anchors from existing html.
+
+
+## Contribute
+
+Pull Requests or issues are welcome!
+
+For example on adding more languages, or perfecting the cases catched.
+
+
+## License
+
+[MIT](/LICENSE)
--- src/linkify.php
+++ src/linkify.php
@@ -1,47 +1,79 @@
 <?php
 
+namespace alsvanzelf\linkify;
+
 class linkify {
 
-private static $regex_utf8_alpha = '\p{L}'; // any (unicode) letter
+protected static $defaults = array(
+	'rel_nofollow' => true,
+	'target_blank' => true,
+	'class_names'  => '',
+);
+
+private $attributes = '';
+private $matchers   = array();
+
+public function __construct(array $options=array()) {
+	$this->prepare_attributes($options);
+	$this->prepare_matchers();
+}
 
 /**
  * creates html links for all link-a-like parts of a text
- * we focus on: a main use case; short and fast code; and code-sharing between php and js
- * we do not try to achieve perfection, although we think we come very close :)
- * 
- * the basics:
- * - anything that starts with http://, https:// or www.
- * - we do not support ftp, email addresses, basic auth, and other edge cases
- * - we do not check much more, for example real tld's
- * - we add http:// for the href when it was not given in the text
- * 
- * things that work:
- * - case insensitive
- * - unicode
- * - paths, query strings, hashes, and other allowed characters
- * - urls starting/ending a line or the whole string
- * - not including punctuation after the url
- * - urls enclosed in brackets
- * - urls with brackets inside the url
- * 
- * things that do not work:
- * - urls which contain one of: @ |
  * 
  * @note the input string should NOT contain html to prevent XSS
  *       afterwards one can not differentiate the added anchors from existing html
  * 
- * @param  string  $string               input (multiline) string
- * @return string                        string with links inside anchors
- * 
- * @see http://www.codinghorror.com/blog/2008/10/the-problem-with-urls.html
+ * @param  string  $plain input string, optionally multiline
+ * @return string         input with links inside anchors
  */
-public static function linkify($string) {
+public function linkify($plain) {
+	$linkified = $plain;
+	
+	foreach ($this->matchers as $matcher) {
+		list($regex, $replacement) = $matcher;
+		$linkified = preg_replace($regex, $replacement, $linkified);
+	}
+	
+	return $linkified;
+}
+
+private function prepare_attributes(array $options) {
+	$attributes = array();
+	
+	if (isset($options['rel_nofollow']) === false || $options['rel_nofollow'] === true) {
+		$attributes[] = 'rel="nofollow"';
+	}
+	
+	if (isset($options['target_blank']) === false || $options['target_blank'] === true) {
+		$attributes[] = 'target="_blank"';
+	}
+	
+	if (empty($options['class_names']) === false && is_string($options['class_names']) === true) {
+		if (strpos($options['class_names'], '$')) {
+			throw new \Exception('can not use $ in class names to prevent mixing with regex replacement');
+		}
+		
+		$attributes[] = 'class="'.$options['class_names'].'"';
+	}
+	
+	if (empty($attributes)) {
+		return;
+	}
+	
+	$this->attributes = ' '.implode(' ', $attributes);
+}
+
+private function prepare_matchers() {
 	/**
 	 * determine what punctuation can be used directly after a url
 	 * if one of these characters ends the url, it is kept outside the anchor
 	 */
 	$regex_punctuation = '\.?!:,;\)\]';
 	
+	/**
+	 * main regex, comments are inline
+	 */
 	$regex = '{
 		# before the url should be: start of string; whitespace; or opening bracket ($1)
 		(^|[\(\[\s]+)
@@ -65,7 +97,7 @@ public static function linkify($string) {
 				(www\.)?
 				
 				# allowed characters: alphanum (unicode), path, query string, hash and some others
-				[-'.self::$regex_utf8_alpha.'0-9\./?=&#%+~_()\[\]!:,.;]+
+				[-\p{L}0-9\./?=&#%+~_()\[\]!:,.;]+
 				
 				# url should not end with whitespace or punctuation
 				# this way we can separate that from the anchor
@@ -79,9 +111,9 @@ public static function linkify($string) {
 	# modifiers: case insensitive; multiline; allow whitespace and comments; use unicode
 	}imxu';
 	
-	$replacement = '$1<a href="http$5://$6$7" rel="nofollow" target="_blank">$2</a>$9';
+	$replacement = '$1<a href="http$5://$6$7"'.$this->attributes.'>$2</a>$9';
 	
-	$output = preg_replace($regex, $replacement, $string);
+	$this->matchers[] = array($regex, $replacement);
 	
 	/**
 	 * brackets are tricky characters as they are allowed inside urls but often used around urls
@@ -93,9 +125,10 @@ public static function linkify($string) {
 	 *       doesn't work: "(http://en.wikipedia.nl/wiki/foo_(bar)" (the closing bracket will be added to the url)
 	 *       does work:    "(http://en.wikipedia.nl/wiki/foo_(bar))" (only the first closing bracket will be added)
 	 */
-	$fixed = preg_replace('{(<a href=".+)(" rel="nofollow" target="_blank">.+)(\(|\[)(.*?)(</a>)(\)|\])}imu', '$1$6$2$3$4$6$5', $output);
+	$regex = '{(<a href=".+)("'.$this->attributes.'>.+)(\(|\[)(.*?)(</a>)(\)|\])}imu';
+	$replacement = '$1$6$2$3$4$6$5';
 	
-	return $fixed;
+	$this->matchers[] = array($regex, $replacement);
 }
 
 }
